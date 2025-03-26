ee233822980b7a43a2b8a04424940be4fbf04337
Sb93 <66685463+Shadowblazer93@users.noreply.github.com>
2025-03-26 13:14:47 +0800

    inital

---
+++ User.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class User {
+    private String UserID;
+    private String password;
+
+    public void changePassword() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter new password: ");
+        String newPassword = sc.next();
+        
+        this.password = newPassword;
+        System.out.println("Password successfully changed.");
+    }
+}
