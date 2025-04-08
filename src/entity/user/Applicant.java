8994ad272941f9443638ba6dc151d3d29c7fa6c3
Sb93 <66685463+Shadowblazer93@users.noreply.github.com>
2025-04-08 20:52:40 +0800

    Chaange application enquiry list to arraylist

--- src/entity/user/Applicant.java
+++ src/entity/user/Applicant.java
@@ -2,21 +2,28 @@ package entity.user;
 
 import entity.enquiry.Enquiry;
 import entity.project.BTOProject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import enums.UserRole;
+
 import java.util.Scanner;
 
 public class Applicant extends User { 
     private BTOProject appliedProject;
     private String applicationStatus;
     private String flatType;
-    private Enquiry Enquiries[];
+    private List<Enquiry> enquiries;
+    private UserRole role = UserRole.APPLICANT;
     private int maxEnqId = 0;
 
-    public Applicant(BTOProject appliedProject, String applicationStatus, String flatType,
-    Enquiry Enquiries[], int maxEnqID){
+    public Applicant(String UserID, String name, String password, BTOProject appliedProject, String applicationStatus, String flatType, int maxEnqID){
+        super(UserID, name, password);
         this.appliedProject = appliedProject;
         this.applicationStatus = applicationStatus;
         this.flatType = flatType;
-        this.Enquiries = Enquiries;
+        this.enquiries = new ArrayList<>();
         this.maxEnqId = maxEnqID;
     }
 
@@ -46,17 +53,22 @@ public class Applicant extends User {
         System.out.print("Enter ID of enquiry you want to view: ");
         int enqId = sc.nextInt();
         sc.close();
-        Enquiry enq = null;
-        boolean found = false;
+        // Enquiry enq = null;
+        // boolean found = false;
 
         // find enquiry
-        for (int i=0;i<Enquiries.length;i++) {
-            if (Enquiries[i].id==enqId) enq = Enquiries[i];
-            found = true;
-        }
+        // for (int i=0;i<enquiries.size();i++) {
+        //     if (Enquiries[i].id==enqId) enq = Enquiries[i];
+        //     found = true;
+        // }
+
+        Enquiry enq = enquiries.stream()
+            .filter(e -> e.id==enqId)
+            .findFirst()
+            .orElse(null);
 
         // check if enquiry exists
-        if (!found) {
+        if (enq==null) {
             System.out.println("Could not find an enquiry with that ID!");
             return;
         }
@@ -70,17 +82,21 @@ public class Applicant extends User {
         System.out.print("Enter ID of enquiry you want to view: ");
         int enqId = sc.nextInt();
         sc.close();
-        Enquiry enq = null;
-        boolean found = false;
+        // Enquiry enq = null;
+        // boolean found = false;
 
         // find enquiry
-        for (int i=0;i<Enquiries.length;i++) {
-            if (Enquiries[i].id==enqId) enq = Enquiries[i];
-            found = true;
-        }
+        // for (int i=0;i<Enquiries.length;i++) {
+        //     if (Enquiries[i].id==enqId) enq = Enquiries[i];
+        //     found = true;
+        // }
+        Enquiry enq = enquiries.stream()
+            .filter(e -> e.id==enqId)
+            .findFirst()
+            .orElse(null);
 
         // check if enquiry exists
-        if (!found) {
+        if (enq==null) {
             System.out.println("Could not find an enquiry with that ID!");
             return;
         }
@@ -93,23 +109,29 @@ public class Applicant extends User {
         System.out.print("Enter ID of enquiry you want to view: ");
         int enqId = sc.nextInt();
         sc.close();
-        Enquiry enq = null;
+        // Enquiry enq = null;
         int foundId=-1;
 
         // find enquiry
-        for (int i=0;i<Enquiries.length;i++) {
-            if (Enquiries[i].id==enqId) {
-                foundId = i;
-                break;
-            }
-        }
+        // for (int i=0;i<Enquiries.length;i++) {
+        //     if (Enquiries[i].id==enqId) {
+        //         foundId = i;
+        //         break;
+        //     }
+        // }
+
+        Enquiry enq = enquiries.stream()
+            .filter(e -> e.id==enqId)
+            .findFirst()
+            .orElse(null);
 
         // check if enquiry exists
-        if (foundId==-1) {
+        if (enq==null) {
             System.out.println("Could not find an enquiry with that ID!");
             return;
         }
 
-        Enquiries[foundId] = null;
+        // Enquiries[foundId] = null;
+        enquiries.remove(enq);
     }
 }
