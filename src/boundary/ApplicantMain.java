c5cd0029eb0c68b0fe1759e488c8f02c31d86eeb
Sb93 <66685463+Shadowblazer93@users.noreply.github.com>
2025-04-09 14:26:37 +0800

    applicantMain

--- src/boundary/ApplicantMain.java
+++ src/boundary/ApplicantMain.java
@@ -1,15 +1,19 @@
 package boundary;
 
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
+import controller.BTOProjectController;
+import entity.project.BTOProject;
 import entity.user.Applicant;
 
 public class ApplicantMain {
-    public ApplicantMain(Applicant applicant) {
+    public ApplicantMain(Applicant applicant, BTOProjectController projectController) {
         Scanner sc = new Scanner(System.in);
         int choice = 0;
 
-        while (choice!=7) {
+        while (choice!=8) {
             System.out.printf("""
                     Hi %s
                     ------------------------------
@@ -17,37 +21,83 @@ public class ApplicantMain {
                     ------------------------------
                     1. View project list
                     2. Apply to project
-                    3. Withdraw from Project
-                    4. Submit Enquiry
-                    5. Edit Enquiry
-                    6. Delete Enquiry
-                    7. Logout
+                    3. View Applied project
+                    4. Withdraw from Project
+                    5. Submit Enquiry
+                    6. Edit Enquiry
+                    7. Delete Enquiry
+                    8. Logout
                     ------------------------------
                     """, applicant.getName());
             
             choice = sc.nextInt();
+            sc.nextLine();
 
             switch (choice) {
                 case 1:
-                    // read from csv and filter for flatType
+                    Set<String> projects = projectController.getAllProjects().keySet();
+                    System.out.println("Project list:");
+
+                    for (String projectName : projects) {
+                        BTOProject proj = projectController.getAllProjects().get(projectName);
+                        // only allow projects based on age and marital status to be visible
+                        System.out.println(proj);
+                    }
+                    
                     break;
             
                 case 2:
+                    System.out.println("Enter name of project:");
+                    String projectName = sc.next();
+                    // Check if project exists
+                    // Check if project is open
+                    // Check if project is not full
+                    // Check if applicant has not applied to any other project
+                    // Check if applicant is eligible
+                    // Apply to project
                     break;
 
                 case 3:
-                    break;
+                    BTOProject appliedProject = applicant.getAppliedProject();
 
-                case 4:
+                    if (appliedProject==null) {System.out.println("You have not applied to any project.");}
+                    else {System.out.println("Applied project:\n" + appliedProject);}
+
                     break;
                 
+                case 4:
+                    BTOProject projectToWithdraw = applicant.getAppliedProject();
+                    if (projectToWithdraw==null) {System.out.println("You have not applied to any project.");}
+                    else {
+                        applicant.projectWithdraw(projectToWithdraw);
+                    }
+                    break;
+
                 case 5:
+                    System.out.println("Enter enquiry message: ");
+                    String msg = sc.nextLine();
+                    applicant.enquirySubmit(msg);
                     break;
                 
                 case 6:
+                    System.out.print("Enter submitted enquiry ID: ");
+                    int enqid = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter new message: ");
+                    String newMsg = sc.nextLine();
+                    applicant.enquiryEdit(enqid,newMsg);
                     break;
                 
                 case 7:
+                    System.out.print("Enter submitted enquiry ID: ");
+                    int enqId = sc.nextInt();
+                    sc.nextLine();
+                    applicant.enquiryDelete(enqId);
+                    break;
+                
+                case 8:
+                    System.out.println("Logging out...");
+                    sc.close();
                     return;
 
                 default:
