0ee57a06b3d432237d8ba2623bcd0fd792d62d5b
Sb93 <66685463+Shadowblazer93@users.noreply.github.com>
2025-04-09 15:27:32 +0800

    csvprojects

--- src/database/ReadCSV.java
+++ src/database/ReadCSV.java
@@ -1,8 +1,11 @@
 package database;
+import java.io.File;
+import java.util.Scanner;
+import entity.project.BTOProject;
 
 public class ReadCSV {
     public void loadManager() {
-        
+
     }
 
     public void loadOfficer() {
@@ -13,8 +16,18 @@ public class ReadCSV {
 
     }
 
-    public void loadProject() {
-
+    public List<BTOProject> loadProject() {
+        File projectsFile = new File("src/database/projects.csv");
+        Scanner projectsReader = new Scanner(projectsFile);
+        while (projectsReader.hasNextLine()) {
+            String data = projectsReader.nextLine();
+            String[] projectData = data.split(",");
+            String projectName = projectData[0];
+            String projectLocation = projectData[1];
+            String projectType = projectData[2];
+            int projectSize = Integer.parseInt(projectData[3]);
+            String projectStatus = projectData[4];
+        }
     }
 
     public void loadEnquiry() {
