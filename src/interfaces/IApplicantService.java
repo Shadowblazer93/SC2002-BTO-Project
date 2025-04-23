239a1fa07e4ec0112b6718b58f754fa384bfdba9
hongyu <lhy.lim.hong.yu@gmail.com>
2025-04-23 16:32:09 +0800

    Delete duplicated javadoc

--- src/interfaces/IApplicantService.java
+++ src/interfaces/IApplicantService.java
@@ -22,16 +22,6 @@ public interface IApplicantService {
      * @param password      The password for the applicant's account.
      * @return The newly created Applicant object.
      */
-    
-    /**
-     * Create a new applicant with the specified details
-     * @param nric Applicant NRIC
-     * @param name Applicant name
-     * @param age Applicant age
-     * @param maritalStatus Applicant marital status
-     * @param password Applicant password
-     * @return Newly created applicant object
-     */
     Applicant createApplicant(String nric, String name, int age, String maritalStatus, String password);
 
     /**
@@ -40,23 +30,12 @@ public interface IApplicantService {
      * @param nric The NRIC of the applicant to retrieve.
      * @return The Applicant object associated with the given NRIC, or null if no such applicant exists.
      */
-    
-    /**
-     * Retrieve an applicant using their NRIC
-     * @param nric Applicant NRIC
-     * @return Applicant object if found, null otherwise
-     */
     Applicant getApplicant(String nric);
 
     /**
      * Retrieves all applicants in the system.
      *
      * @return A map of all applicants, keyed by their NRIC.
-     */
-    
-    /**
-     * Retrieve a map of all Applicants
-     * @return Map of NRICs to Applicant objects
      */
     Map<String, Applicant> getAllApplicants();
 }
